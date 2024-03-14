103cb403b7a5d253dff2d7502d845d130ebb1b7e
Girish Nair <nairgirish100@gmail.com>
2024-03-14 12:30:02 +0400

    link updates done

--- app/page.tsx
+++ app/page.tsx
@@ -46,8 +46,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+          <h2 className={`mb-3 text-3xl font-bold`}>
+            Documents{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
