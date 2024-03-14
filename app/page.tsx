e2728f36362c4ddfe6b5350cf15707ed5e4b445d
Girish Nair <nairgirish100@gmail.com>
2024-03-14 12:00:00 +0400

    page updates done

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={200}
+              height={44}
               priority
             />
           </a>
@@ -33,8 +33,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={250}
+          height={60}
           priority
         />
       </div>
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find all the in-depth information about Next.js features and API.
           </p>
         </a>
 
